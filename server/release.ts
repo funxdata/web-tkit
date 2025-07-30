713b668c70879f64325b6aea74ab9a5c4b42fe37
git <client@funxdata.com>
2025-07-30 12:03:57 +0800

    fix release dir

--- server/release.ts
+++ server/release.ts
@@ -2,23 +2,22 @@ import { resolve, dirname, fromFileUrl } from "@std/path";
 import { uploadFileToUpyun, readFilesRecursive } from "./upfiles.ts";
 
 // 发布的目录
-const public_dir = Deno.args[0];
-const stat = await Deno.stat(public_dir).catch(() => null);
-if (!stat?.isFile) {
-  console.log(`${public_dir} not found`);
-  Deno.exit(1); // 可选
-} 
-
-// 发布到的路径
-const cloud_dir = Deno.args[1];
-
+const args_src = Deno.args[0];
 // 获取当前模块的目录路径
 const __dirname = dirname(fromFileUrl(import.meta.url));
 console.log(__dirname);
 
 // 示例用法
-const publicDir = resolve(__dirname, public_dir);
+const publicDir = resolve(__dirname, args_src);
 console.log(publicDir);
+const stat = await Deno.stat(publicDir).catch(() => null);
+if (!stat?.isFile) {
+  console.log(`${publicDir} not found`);
+  Deno.exit(1); // 可选
+} 
+
+// 发布到的路径
+const cloud_dir = Deno.args[1];
 
 // 读取所有文件
 const dir_files = await readFilesRecursive(publicDir);
