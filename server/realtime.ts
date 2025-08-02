f04390f2f855306c1b953a4486159fc9214f53fb
git <client@funxdata.com>
2025-08-02 23:31:13 +0800

    update pack ts console

--- server/realtime.ts
+++ server/realtime.ts
@@ -1,7 +1,6 @@
 import { bundle } from "@deno/emit";
 const importMapText = await Deno.readTextFile("./deno.json");
 const importMap = JSON.parse(importMapText);
-console.log(importMap.imports);
 // deno-lint-ignore ban-types
 export const  real_time_info =async (file_src:string):Promise<String>=>{
     const result = await bundle(file_src, {
